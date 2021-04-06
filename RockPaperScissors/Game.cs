9c34a12e72a53ca7464325905e7a60a3a787abfe
maxrabets <rabetsmaxim@gmail.com>
2021-04-06 14:44:30 +0300

    .

---
+++ RockPaperScissors/Game.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace RockPaperScissors
+{
+    public class Game
+    {
+        private RandomNumberGenerator generator = RandomNumberGenerator.Create();
+        public byte[] Key { get; private set; }
+        private byte[] computerMoveBytes = new byte[4];
+        public int ComputerMove { get; private set; }
+        public byte[] Hash { get; private set; }
+        public string[] Items { get; set; }
+
+        public Game(string[] items)
+        {
+            Items = items;
+            Key = new byte[16];
+        }
+
+        public static bool CheckItems(string[] items)
+        {
+            if (items.Length < 3 || items.Length % 2 == 0 
+                || items.Where(item => items.Count(i => i == item) > 1).Count() > 1)
+                return false;
+            return true;
+        }
+
+        public int MakeComputerMove()
+        {
+            generator.GetNonZeroBytes(Key);
+            generator.GetNonZeroBytes(computerMoveBytes);
+            ComputerMove = BitConverter.ToInt32(computerMoveBytes, 0);
+            ComputerMove = Math.Abs(ComputerMove % Items.Length);
+            computerMoveBytes = BitConverter.GetBytes(ComputerMove);
+            HMACSHA256 hmac = new HMACSHA256(Key);
+            Hash = hmac.ComputeHash(computerMoveBytes);
+            return ComputerMove;
+        }
+        
+        public int CompareMoves(int move1, int move2)
+        {
+            if (move1 == move2)
+                return 0;
+            int half = Items.Length / 2;
+            if(move1 > move2)
+            {
+                if(move1 - move2 <= half)
+                    return 1;
+                else
+                    return 2;
+            }
+            else
+            {
+                if (move2 - move1 <= half)
+                    return 2;
+                else
+                    return 1;
+            }
+        }
+    }
+}
---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(!Game.CheckItems(args))
+            {
+                Console.WriteLine("\r\nNumber of items shoud be odd and grater than 3." +
+                    " All items should be unique");
+                Console.WriteLine("Example: RockPaperScissors.exe rock paper scissors lizard Spock");
+                Console.ReadLine();
+                return;
+            }
+
+            Game game = new Game(args);
+            int computerMove = game.MakeComputerMove();
+
+            Console.WriteLine("HMAC: " + BitConverter.ToString(game.Hash));
+            Console.WriteLine("\r\nAvailable moves: ");
+            for(int i = 0; i < game.Items.Length; i++)
+                Console.WriteLine($"{i+1} - {game.Items[i]}");
+            Console.WriteLine("0 - exit");
+
+            while (true)
+            {
+                Console.Write("\r\nENTER YOUR MOVE: ");
+                if (int.TryParse(Console.ReadLine(), out int playerMove))
+                {
+                    if (playerMove == 0)
+                        return;
+                    if(playerMove > game.Items.Length || playerMove < 0)
+                    {
+                        Console.WriteLine("\r\nBad move. Please, choose one of this: ");
+                        for (int i = 0; i < game.Items.Length; i++)
+                            Console.WriteLine($"{i + 1} - {game.Items[i]}");
+                        Console.WriteLine("0 - exit");
+                        continue;
+                    }
+
+                    playerMove--;
+                    Console.Write("YOUR MOVE: " + game.Items[playerMove]);
+                    Console.Write("\r\nCOMPUTER MOVE: " + game.Items[computerMove]);
+                    Console.WriteLine();
+                    int result = game.CompareMoves(playerMove, computerMove);
+                    switch (result)
+                    {
+                        case 1:
+                            Console.WriteLine("you win");
+                            break;
+
+                        case 2:
+                            Console.WriteLine("YOU LOOOOOOOOSE!!!!!!!");
+                            break;
+
+                        case 0:
+                            Console.WriteLine("Draw");
+                            break;
+                    }
+                    Console.WriteLine("\r\nHMAC key: " + BitConverter.ToString(game.Key));
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("\r\nBad move. Please, choose one of this: ");
+                    for (int i = 0; i < game.Items.Length; i++)
+                        Console.WriteLine($"{i + 1} - {game.Items[i]}");
+                    Console.WriteLine("0 - exit");
+                }
+            } 
+        }
+
+    }
+}
